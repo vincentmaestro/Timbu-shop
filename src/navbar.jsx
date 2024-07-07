1216be67f185908ea9a60a608721a10b098fb6d7
vincentmaestro <maestroobika@gmail.com>
2024-07-08 00:06:21 +0100

    updates

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,16 +1,30 @@
 import { Link, NavLink } from "react-router-dom";
 function Navbar () {
+
+    function goHome(e) {
+        e.target.parentElement.parentElement.childNodes.forEach(node => {
+            node.firstChild.classList.remove('active-link');
+        });
+    }
+
+    function styleNavLink(e) {
+        e.target.parentElement.parentElement.childNodes.forEach(node => {
+            node.firstChild.classList.remove('active-link');
+        });
+        e.target.classList.add('active-link');
+    }
+
     return (
         <nav>
             <i className="ri-menu-line menu"></i>
 
             <ul className="left">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/">About us</Link></li>
-                <li><Link to="/shop">Shop</Link></li>
+                <li><Link to="/" onClick={e => goHome(e)}>Home</Link></li>
+                <li><Link to="/" onClick={e => styleNavLink(e)}>About us</Link></li>
+                <li><Link to="/shop" onClick={e => styleNavLink(e)}>Shop</Link></li>
             </ul>
 
-            <h1 className="logo"><Link to="/">Timbushop</Link></h1>
+            <h1 className="logo">Timbushop</h1>
             
             <div className="right">
                 <i className="ri-search-line"></i>

0a489db1ad0fbd2e5e979d3046db6d11f21b57d1
vincentmaestro <maestroobika@gmail.com>
2024-07-08 01:50:14 +0100

    updates

--- src/footer.jsx
+++ src/footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <footer>
             <div className="left">
-                <h1 className="logo"><Link to="/">Timbushop</Link></h1>
+                <h1 className="logo">Timbushop</h1>
                 <p>Be part of Timbushop community and stay connected with the latest fashion trends and exclusive offers. Follow us on social media and sign up for our newsletter to receive updates on new arrivals and special promotions.</p>
                 <div className="social-media-icons">
                     <Link to="https://wa.me/+2349023695126" target="blank"><i className="ri-whatsapp-line"></i></Link>
--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,10 +1,23 @@
 import { Link, NavLink } from "react-router-dom";
 function Navbar () {
 
+    function toggleMenu() {
+        if(document.querySelector('nav').querySelector('.left').classList.contains('menu-closed')) {
+            document.querySelector('nav').querySelector('.left').classList.remove('menu-closed');
+            document.querySelector('nav').querySelector('.left').classList.add('menu-open');
+        }
+        
+        else if(document.querySelector('nav').querySelector('.left').classList.contains('menu-open')) {
+            document.querySelector('nav').querySelector('.left').classList.remove('menu-open');
+            document.querySelector('nav').querySelector('.left').classList.add('menu-closed');
+        }
+    }
+
     function goHome(e) {
         e.target.parentElement.parentElement.childNodes.forEach(node => {
             node.firstChild.classList.remove('active-link');
         });
+        document.querySelector('nav').querySelector('.left').classList.add('menu-closed');
     }
 
     function styleNavLink(e) {
@@ -12,13 +25,14 @@ function Navbar () {
             node.firstChild.classList.remove('active-link');
         });
         e.target.classList.add('active-link');
+        document.querySelector('nav').querySelector('.left').classList.add('menu-closed');
     }
 
     return (
         <nav>
-            <i className="ri-menu-line menu"></i>
+            <i className="ri-menu-line menu" onClick={() => toggleMenu()}></i>
 
-            <ul className="left">
+            <ul className="left menu-closed">
                 <li><Link to="/" onClick={e => goHome(e)}>Home</Link></li>
                 <li><Link to="/" onClick={e => styleNavLink(e)}>About us</Link></li>
                 <li><Link to="/shop" onClick={e => styleNavLink(e)}>Shop</Link></li>
